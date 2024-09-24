1c5dbb4482b63c278438c7fa9b4f11a3315d8260
odd <odd@vanillacoding.co>
2024-09-24 11:13:32 +0900

    Revise unnecessary lint error cases

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Popular from "../Popular";
 import Battle from "../Battle";
 import NavButton from "../NavButton";
--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Popular() {
   return <h1 className="center-text">This is Popular!</h1>;
 }
